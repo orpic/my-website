b91a7e87a8124ca5fc784e57295fa9430011a25d
Shobhit <121398721+orpic@users.noreply.github.com>
2023-07-02 10:25:28 +0530

    Streak

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="ml-1 text-3xl font-bold underline">Shobhit</h1>
+      <h1 className="ml-1 text-3xl font-bold underline">Coming Soon</h1>
     </>
   );
 }
